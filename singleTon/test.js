d7f5ae66ca27cdd73b3396ce0b4379780f00384e
kanoLano <t.kano.624@gmail.com>
2015-06-13 19:04:50 +0900

    インスタンスを複数生成できてしまう方法を見つけたので改修。
    Signed-off-by: kanoLano <t.kano.624@gmail.com>

--- singleTon/test.js
+++ singleTon/test.js
@@ -1,21 +1,34 @@
+/**
+ * シングルトンで実装されたHogeクラス
+ */
 var Hoge = (function () {
+    /**
+      * コンストラクタ
+      */
     function Hoge(caller) {
-        if (caller === void 0) { caller = null; }
-        if (caller == Hoge.getInstance) {
-            console.log("グッド！");
-        }
-        else {
-            throw new Error("なっ、何をするだアア！");
-        }
+        if (caller == Hoge.getInstance)
+            console.log("☆2 インスタンスを作成。一度しか呼ばれない。");
+        else if (Hoge._instance)
+            throw new Error("既にインスタンスが存在するためエラー。");
+        else
+            throw new Error("コンストラクタの引数が不正な為エラー。");
     }
+    /**
+     * インスタンスの取得
+     */
     Hoge.getInstance = function () {
-        if (!this._instance) {
+        console.log("☆1 Hogeクラスインスタンスの取得");
+        // インスタンスが存在しない場合、new演算子でインスタンスを作成
+        // 引数に渡すのは、Hoge.getInstanceというFunction
+        if (!this._instance)
             this._instance = new Hoge(Hoge.getInstance);
-        }
+        // 生成済みのインスタンスを返す
         return this._instance;
     };
     return Hoge;
 })();
-Hoge.getInstance(); // グッド！を出力
-new Hoge(); // 直接インスタンス化できないためエラー
+// ☆1と☆2のログが出力。Hogeクラスのインスタンスがnewで生成される
+var hoge1 = Hoge.getInstance();
+// ☆1のログのみ出力。インスタンスはnewで生成されない
+var hoge2 = Hoge.getInstance();
 //# sourceMappingURL=test.js.map
--- singleTon/test.ts
+++ singleTon/test.ts
@@ -1,27 +1,46 @@
+/**
+ * シングルトンで実装されたHogeクラス
+ */
 class Hoge
 {
+    /**
+     *  インスタンス
+     */
     private static _instance:Hoge;
 
+    /**
+     * インスタンスの取得
+     */
     public static getInstance():Hoge
     {
+        console.log("☆1 Hogeクラスインスタンスの取得");
+        // インスタンスが存在しない場合、new演算子でインスタンスを作成
+        // 引数に渡すのは、Hoge.getInstanceというFunction
         if (!this._instance)
-        {
             this._instance = new Hoge(Hoge.getInstance);
-        }
+
+        // 生成済みのインスタンスを返す
         return this._instance;
     }
 
-    constructor(caller:Function = null) {
+
+   /**
+     * コンストラクタ
+     */
+    constructor(caller:Function) {
         if (caller == Hoge.getInstance)
-        {
-            console.log("グッド！");
-        }
+            console.log("☆2 インスタンスを作成。一度しか呼ばれない。");
+        else if (Hoge._instance)
+            throw new Error("既にインスタンスが存在するためエラー。");
         else
-        {
-            throw new Error("なっ、何をするだアア！");
-        }
+            throw new Error("コンストラクタの引数が不正な為エラー。");
     }
 }
 
-Hoge.getInstance(); // グッド！を出力
-new Hoge(); // 直接インスタンス化できないためエラー
+// ☆1と☆2のログが出力。Hogeクラスのインスタンスがnewで生成される
+var hoge1:Hoge = Hoge.getInstance();
+// ☆1のログのみ出力。インスタンスはnewで生成されない
+var hoge2:Hoge = Hoge.getInstance();
+// new Hoge()ではインスタンス化不可能
+// var hoge:Hoge = new Hoge();
+
