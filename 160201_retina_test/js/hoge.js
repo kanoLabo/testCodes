c767c9feb30ff539ce3b16df61b12f1fb59ca349
tonkotsuboy <t.kano.624@gmail.com>
2017-05-01 10:41:40 +0900

    retina display test

---
+++ 160201_retina_test/js/hoge.js
@@ -0,0 +1,13 @@
+var consoleTsumikiLogo = function(str) {
+  var styles = 'background: url(http://blog.tsumikiinc.com/img/t.png) no-repeat center 0;' +
+    'background-size: 100% auto;' +
+    'font-family: "Source Code Pro", monospace;' +
+    'font-size: 10px;' +
+    'font-size: 10px;' +
+    'color: red;' +
+    'line-height: 120px;' +
+    'padding: 44px 64px 44px 0;';
+
+
+  console.log('%c' + str, styles);
+};
