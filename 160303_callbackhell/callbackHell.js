85f567269882011ed1baaf6c4815356b8a857d93
tonkotsuboy <t.kano.624@gmail.com>
2017-05-01 10:40:22 +0900

    callback hell

---
+++ 160303_callbackhell/callbackHell.js
@@ -0,0 +1,116 @@
+var count = 0;
+
+function hell(callBack) {
+	count++;
+	alert(count + "回目:蝋人形にしてやろうかァ！");
+	if (callBack)
+		callBack();
+}
+
+hell(
+	hell(
+		hell(
+			hell(
+				hell(
+					hell(
+						hell(
+							hell(
+								hell(
+									hell(
+										hell(
+											hell(
+												hell(
+													hell(
+														hell(
+															hell(
+																hell(
+																	hell(
+																		hell(
+																			hell(
+																				hell(
+																					hell(
+																						hell(
+																							hell(
+																								hell(
+																									hell(
+																										hell(
+																											hell(
+																												hell(
+																													hell(
+																														hell(
+																															hell(
+																																hell(
+																																	hell(
+																																		hell(
+																																			hell(
+																																				hell(
+																																					hell(
+																																						hell(
+																																							hell(
+																																								hell(
+																																									hell(
+																																										hell(
+																																											hell(
+																																												hell(
+																																													hell(
+																																														hell(
+																																															hell(
+																																																hell(
+																																																	hell(
+																																																		hell(
+																																																			hell(
+																																																				hell(
+
+																																																				)
+																																																			)
+																																																		)
+																																																	)
+																																																)
+																																															)
+																																														)
+																																													)
+																																												)
+																																											)
+																																										)
+																																									)
+																																								)
+																																							)
+																																						)
+																																					)
+																																				)
+																																			)
+																																		)
+																																	)
+																																)
+																															)
+																														)
+																													)
+																												)
+																											)
+																										)
+																									)
+																								)
+																							)
+																						)
+																					)
+																				)
+																			)
+																		)
+																	)
+																)
+															)
+														)
+													)
+												)
+											)
+										)
+									)
+								)
+							)
+						)
+					)
+				)
+			)
+		)
+	)
+);
